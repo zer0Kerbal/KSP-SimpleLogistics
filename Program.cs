355aeeae7ab7f62327637150185374d631ca8470
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2019-06-23 05:30:18 -0500

    that 'aha' moment
    set local Env$ to KSP directory, and updated .csproj to reflect. many, many, many errors went away.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KSP_SimpleLogistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
