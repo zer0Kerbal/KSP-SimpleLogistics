c97b36b494476d537a78dbf70f64df9227ba1d7d
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-03-21 00:36:31 -0500

    2.0.4.0

--- Source/appropriateSituation.cs
+++ Source/appropriateSituation.cs
@@ -1,139 +1,159 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+using KSP.UI.Screens;
+using KSP.IO;
+using KSP.Localization;
+using KSPAssets;
+using System.Linq;
 using UnityEngine;
 
-namespace simplelogistics
+namespace SimpleLogistics
 {
-  //? either set in part.cfg or in game.settings
-  // consitionPermissions
-  const yesLanded = true,
-        yesSplashed = true,
-        yesPreLaunch = true,
-        yesFlying = false,
-        yesSubOrbital = false,
-        yesOrbiting = false,
-        yesEscaping = false,
-        yesDocked = true,
-        yesNone = false,
-        yesPartialUnmaned = true,
-        yesPartialManned = true,
-        yesFull = true;
-
-  double  maxRange = 2400,
-          maxAltitude = 500,
-          maxGroundSpeed = 20;
-
-  //TODO: need a way to trap changes to conditionPerissions in game.settins; 
-
-  //! from KSP
-  internal enum Situations
-  {
-    LANDED,
-    SPLASHED,
-    PRELAUNCH,
-    FLYING,
-    SUB_ORBITAL,
-    ORBITING,
-    ESCAPING,
-    DOCKED
-  };
-
-  //! from KSP
-  internal enum ControlLevel
-  {
-    NONE,
-    PARTIAL_UNMANNED,
-    PARTIAL_MANNED,
-    FULL
-  };
-
-  internal boolean appropriateRange
-  {
-    if ( nearestVessel <= maxRange) return true
-    else return false;
-  }
-
-  internal boolean appropriateAltitude
-  {
-    if (vessel.radarAltitude <= maxAltitude) return true
-    else return false;
-  }
-
-  internal boolean appropriateGroundSpeed
-  {
-    if (vessel.groundSpeed <= maxGroundSpeed) return true
-    else return false;
-  }
-
-  internal boolean appropriateControlLevel
-  {
-    switch (ControlLevel controlLevel = Vessel.ControlLevel)
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class Util : MonoBehaviour
     {
-      case controlLevel.FULL:
-      Log.msg("control level: {0}", controlLevel.ToString());
-        if (yesFULL) return True;
-        break;
-      case controlLevel.PARTIAL_MANNED:
-      Log.msg("control level: {0}", controlLevel.ToString());
-        if (yesFULL) return True;
-        break;
-      case controlLevel.PARTIAL_UNMANNED:
-      Log.msg("control level: {0}", controlLevel.ToString());
-        if (yesFULL) return True;
-        break;
-      case controlLevel.NONE:
-      Log.msg("control level: {0}", controlLevel.ToString());
-        if (yesFULL) return True;
-        break;
-    }
-    return false;
-  }
+        #region Enums Vars
+        //? either set in part.cfg or in game.settings
+        /* TODO: 
+         * need a way to trap changes to conditionPerissions in game.settins; 
+         * InRange()
+         * AppropriateCondition()
+         * AppropriateState()
+         * AppropriateAltitude()
+         * NetworkEligible()
+         * IsActive needs to be read in from part and set IsActive flag
+        
+        // consitionPermissions
+        const bool yesLanded = true,
+                   yesSplashed = true,
+                   yesPreLaunch = true,
+                   yesFlying = false,
+                   yesSubOrbital = false,
+                   yesOrbiting = false,
+                   yesEscaping = false,
+                   yesDocked = true,
 
-  boolean appropriateSituation
-  {
-    switch (Situations situation = Vessel.Situation)
-    {
-      case situation.LANDED:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesLanded.ToString());
-        if (yesLanded) return True;
-        break;
-      case situation.SPLASHED:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesSplashed.ToString());
-        if (yesLanded) return True;
-        break;
-      case situation.PRELAUNCH:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesPreLaunch.ToString());
-        if (yesLanded) return True;
-        break;
-      case situation.FLYING:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesFlying.ToString());
-        if (yesLanded) return True;
-        break;
-      case situation.SUB_ORBITAL:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesSubOrbital.ToString());
-        if (yesLanded) return True;
-        break;
-      case situation.ORBITING:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesOrbiting.ToString());
-        if (yesLanded) return True;
-        break;
-      case situation.ESCAPING:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesEscaping.ToString());
-        if (yesLanded) return True;
-        break;
-      case situation.DOCKED:
-        Log.msg("situation {0} permission {1}", situation.ToString(), yesDocked.ToString());
-        if (yesLanded) return True;
-        break;
+                   yesNone = false,
+                   yesPartialUnmaned = true,
+                   yesPartialManned = true,
+                   yesFull = true;
+
+        const double maxRange = 2400,
+                     maxAltitude = 500,
+                     maxGroundSpeed = 20;
+        //! from KSP
+        internal enum sits : byte { LANDED, SPLASHED, PRELAUNCH, FLYING, SUB_ORBITAL, ORBITING, ESCAPING, DOCKED };
+
+        //! from KSP
+        internal enum cLevel : byte { NONE, PARTIAL_UNMANNED, PARTIAL_MANNED, FULL };
+
+        #endregion
+        internal bool InRange(Vessel vessel)
+        {
+     /*       Transform partTransform = part.transform;
+            Vector3 directionToTarget = partTransform.position - kerbalOnEva.vessel.transform.position;
+            double angle = Vector3.Angle(GetPortDirection(), directionToTarget);
+            double distance = directionToTarget.magnitude;
+
+            Log.dbg("Angle " + angle);
+            Log.dbg("Range " + distance);
+
+            // if (Mathf.Abs(angle) < 35 && distance < range) return true;
+            if (distance < maxRange) return true;*/
+            return false;
+        }
+
+        internal bool AppropriateRange()
+        {
+            /* // FlightGlobals.ActiveVessel.vesselRanges .situation != Vessel.Situations.LANDED)
+            if (nearestVessel <= maxRange) return true;
+            else return false;*/
+            return true;
+        }
+
+        internal bool AppropriateAltitude()
+        {
+            if (FlightGlobals.ActiveVessel.radarAltitude <= maxAltitude) return true;
+            else return false;
+        }
+
+        internal bool AppropriateGroundSpeed()
+        {
+            if (FlightGlobals.ActiveVessel.srfSpeed <= maxGroundSpeed) return true;
+            else return false;
+        }
+
+        internal bool AppropriateControlLevel()
+        {
+            Vessel.ControlLevel controlLevel = FlightGlobals.ActiveVessel.CurrentControlLevel;
+            switch (controlLevel)
+            {
+                case Vessel.ControlLevel.FULL:
+                    Log.dbg("control level: {0}", controlLevel.ToString());
+                    if (yesFull) return true;
+                    break;
+                case Vessel.ControlLevel.PARTIAL_MANNED:
+                    Log.dbg("control level: {0}", controlLevel.ToString());
+                    if (yesPartialManned) return true;
+                    break;
+                case Vessel.ControlLevel.PARTIAL_UNMANNED:
+                    Log.dbg("control level: {0}", controlLevel.ToString());
+                    if (yesPartialUnmaned) return true;
+                    break;
+                case Vessel.ControlLevel.NONE:
+                    Log.dbg("control level: {0}", controlLevel.ToString());
+                    if (yesNone) return true;
+                    break;
+            }
+            return false;
+        }
+
+        public bool AppropriateSituation()
+        {
+            Vessel.Situations situation = FlightGlobals.ActiveVessel.situation;
+            switch (situation)
+            {
+                case Vessel.Situations.LANDED:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesLanded.ToString());
+                    if (yesLanded) return true;
+                    break;
+                case Vessel.Situations.SPLASHED:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesSplashed.ToString());
+                    if (yesSplashed) return true;
+                    break;
+                case Vessel.Situations.PRELAUNCH:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesPreLaunch.ToString());
+                    if (yesPreLaunch) return true;
+                    break;
+                case Vessel.Situations.FLYING:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesFlying.ToString());
+                    if (yesFlying) return true;
+                    break;
+                case Vessel.Situations.SUB_ORBITAL:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesSubOrbital.ToString());
+                    if (yesSubOrbital) return true;
+                    break;
+                case Vessel.Situations.ORBITING:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesOrbiting.ToString());
+                    if (yesOrbiting) return true;
+                    break;
+                case Vessel.Situations.ESCAPING:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesEscaping.ToString());
+                    if (yesEscaping) return true;
+                    break;
+                case Vessel.Situations.DOCKED:
+                    Log.dbg("situation {0} permission {1}", situation.ToString(), yesDocked.ToString());
+                    if (yesLanded) { return true; }
+                break;
+            }
+        }
+
+        public bool NetworkEligible()
+        {
+            if (AppropriateSituation() && AppropriateControlLevel() &&  AppropriateAltitude() && AppropriateGroundSpeed()) return true
+            else return false;
+        }
     }
-  }
 }
-
-  //   if (!vessel.LandedOrSplashed && vessel.radarAltitude > 500d)
-  //   {
-  //       //ScreenMessages.PostScreenMessage("Heat Pumps must be landed to use geothermal wells!", 5f,
-  //       //    ScreenMessageStyle.UPPER_CENTER);
-  //       //Shutdown();
-  //       return True;
-  //   }
-  //   else return False;
-  // } 
