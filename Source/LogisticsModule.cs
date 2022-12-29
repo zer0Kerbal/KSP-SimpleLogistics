4fa952ee41f8c5191d834d4a956e71a086daca02
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-12-29 00:35:50 -0600

    Update LogisticsModule.cs

--- Source/LogisticsModule.cs
+++ Source/LogisticsModule.cs
@@ -6,31 +6,31 @@ using KSP.Localization;
 
 namespace SimpleLogistics
 {
-	[Serializable]
-	public class LogisticsModule: PartModule
-	{
+    [Serializable]
+    public class LogisticsModule : PartModule
+    {
         [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "#SimpleLogistics_Module_Plugged", groupName = "SimpleLogistics", //Plugged In?
             groupDisplayName = "SimpleLogistics! v " + Version.Text, groupStartCollapsed = true)]
         public bool _isActive = false;
 
-		[SerializeField]
+        [SerializeField]
         public bool IsActive { get { return _isActive; } }
 
-		public void Set(bool status)
+        public void Set(bool status)
         { _isActive = status; }
 
-		[KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#SimpleLogistics_Module_PluggedNet", groupName = "SimpleLogistics")] //Plug into Network
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#SimpleLogistics_Module_PluggedNet", groupName = "SimpleLogistics")] //Plug into Network
         internal void Toggle()
         { _isActive = !_isActive; }
 
         public override string ToString()
         { return _isActive ? "false" : "true"; }
 
-#region Var Const Enums
+        #region Var Const Enums
         /// <summary>Module information shown in editors</summary>
         private string info = string.Empty;
 
-#endregion
+        #endregion
         public override string GetInfo()
         {
             //? this is what is shown in the editor
@@ -44,11 +44,11 @@ namespace SimpleLogistics
             }
             return info;
         }
-#region On Event
-        public override void OnStart(PartModule.StartState state) 
+        #region On Event
+        public override void OnStart(PartModule.StartState state)
         {
             Logs.dbg("On Start " + state.ToString());
-		}
+        }
 
         // LGG
         public new void Load(ConfigNode node)
@@ -67,6 +67,6 @@ namespace SimpleLogistics
             node.AddValue("isActive", _isActive);
             base.Save(node); // LGG
         }
-#endregion
+        #endregion
     }
 }
